82fa17100ae0a755d0731b3546a8888fad001019
liaohaipeng <liaohaipeng@chipscoco.com>
2020-08-15 11:31:28 +0800

    coumt

---
+++ liaohaipeng/Defined_Variable_Count.py
@@ -0,0 +1,16 @@
+# 定义变量 12345
+number = 12345
+# 定义权值10**4
+weigth = 10**4
+#
+sum_of_number = number // weigth
+number = number % weigth
+weigth //= 10
+
+while weigth >= 1:
+    sum_of_number += number //weigth
+    number = number % weigth
+    weigth //=10
+print(sum_of_number)
+
+git clone https://OUGREIN:ozy0808.@github.com/OUGREIN/chipscoco-python-learning.git
