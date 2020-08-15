2d35b9b167cd270d4c354c8b57bb7dfcb71a7b37
CZ <chenzhan@chipscoco.com>
2020-08-15 11:31:09 +0800

    error

---
+++ chenzhan/IndentationError.py
@@ -0,0 +1,13 @@
+# __author__ = chenzhan@chipscoco.com
+# __date__ = 2020-08-09
+
+state = 0
+# 判断变量state指向的值是否等于0
+if state == 0:
+    '''
+    如果变量state指向的值为0，
+    就执行print（"开心"）
+    '''
+      print('开心')
+else:
+    print('不开心')
