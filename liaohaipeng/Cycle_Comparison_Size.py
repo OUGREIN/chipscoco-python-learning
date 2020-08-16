8fe293cdcd0e1477f739a5e2e4b39ac86c684c1d
liaohaipeng <liaohaipeng@chipscoco.com>
2020-08-16 17:41:22 +0800

    aaa

---
+++ liaohaipeng/Cycle_Comparison_Size.py
@@ -0,0 +1,11 @@
+numbers = [2,13,5,88]
+max = numbers[0]
+for _ in numbers :
+    if _ > max:
+        max = _
+print(max)
+min = numbers[0]
+for _ in numbers :
+    if _ < min:
+        min =_
+print(min)
