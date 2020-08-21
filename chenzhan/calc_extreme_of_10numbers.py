466b2a2067099c1efd5c07b145b37f4cc6924dab
CZ <chenzhan@chipscoco.com>
2020-08-21 12:02:37 +0800

    add remark

--- chenzhan/calc_extreme_of_10numbers.py
+++ chenzhan/calc_extreme_of_10numbers.py
@@ -4,7 +4,7 @@
 desc:求两个数值型变量的极值
 """
 numbers = [3,4,5,6,7,13,7,110,33,-7]
-# 先定义列表其中一个元素为变量最值
+# 先定义列表其中一个元素为变量最值!!!!...
 max = min = numbers[0]
 for number in numbers:
     max = max if max>number else number
